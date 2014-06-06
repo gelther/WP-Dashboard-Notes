c028e2a9e1891239cbcccb552314e127226a6e48
Sormano <jeroen.sormani@gmail.com>
2014-06-06 20:57:04 +0200

    Deleted debug list

--- wp-dashboard-notes.php
+++ wp-dashboard-notes.php
@@ -127,15 +127,6 @@ class WP_Dashboard_Notes {
 			</div>
 			
 		</div>
-		<ul id="sortable">
-  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 1</li>
-  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 2</li>
-  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 3</li>
-  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 4</li>
-  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 5</li>
-  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 6</li>
-  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 7</li>
-</ul>
  
 		<?php
 				
